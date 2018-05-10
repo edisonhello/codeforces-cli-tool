f73e959c64b5e5fc6e5272e153544354026b3252
edisonhello <edisonhello@hotmail.com>
2018-05-10 14:01:12 +0800

    Add login command
    mv login to codeforces login and change it to Promise

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const readline = require('readline')
 const parseCommand = require('./lib/parseCommand.js')
+const request = require('request')
+const cookieFile = require('tough-cookie-filestore')
+
+let cookiejar = request.jar(new cookieFile('./cookie.json'))
 
 var args = process.argv.splice(2)
 if( args.length > 0 ) {
-    parseCommand(args, false)
+    parseCommand(args, false, cookiejar)
     return
 }
 
@@ -13,9 +17,7 @@ input.on('line', (line) => {
 
     if( args[0] === 'exit') return input.close()
 
-    
-
-    parseCommand(args, true, () => {
+    parseCommand(args, true, cookiejar, () => {
         input.prompt()
     })
 })
