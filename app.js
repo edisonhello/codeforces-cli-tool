fa5e154f20c31586477b58378d68f4dbf83beeaa
edisonhello <edisonhello@hotmail.com>
2020-02-17 01:30:35 +0800

    Refactor some code to fit recent coding style

--- app.js
+++ app.js
@@ -6,20 +6,20 @@ const request = require('request')
 const fs = require('fs')
 
 let args = process.argv.splice(2)
-if( args.length > 0 ) {
+if (args.length > 0) {
     parseCommand(args, false)
     return
 }
 
-let input = readline.createInterface({input: process.stdin, output: process.stdout})
+let input = readline.createInterface({ input: process.stdin, output: process.stdout })
 let busy = false
 input.on('line', (line) => {
-    if(busy) return
+    if (busy) return
     busy = true
 
     let args = line.split(' ')
 
-    if( args[0] === 'exit') return input.close()
+    if (args[0] === 'exit') return input.close()
 
     parseCommand(args, true, () => {
         input.prompt()
